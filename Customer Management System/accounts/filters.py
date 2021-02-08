59334376fa33aecadcfbd345a99827315c73ecbb
Hacnine <hacninetusar@gmail.com>
2021-02-08 09:37:48 +0600

    'Customer Management System'

---
+++ Customer Management System/accounts/filters.py
@@ -0,0 +1,8 @@
+import django_filters
+from .models import *
+#
+#
+# class OrderFilter(django_filters.Filter):
+#     class Meta:
+#         model = Order
+#         fields = '__all__'
--- Customer Management System/accounts/urls.py
+++ Customer Management System/accounts/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('products/', products, name='products'),
     path('customer/<str:pks>/', customer, name='customer'),
     path('customer_list', customer_list, name='customer_list'),
-    path('create_order/', create_order, name='create_order'),
+    path('create_order/<str:pk>/', create_order, name='create_order'),
     path('update_order/<str:order_id>', update_order, name='update_order'),
     path('delete/<int:emp_id>/', delete_data, name='deleted'),
+    # path('mash/', search_customer, name='mash')
 
     # path('update/<int:em_id>/', create_order, name='editupdate'),
 
