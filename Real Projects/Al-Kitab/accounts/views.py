4cfc8797d79b5b8e8e7291d716f958683a8110ae
Hacnine <hacninetusar@gmail.com>
2021-02-28 06:47:28 +0600

    'Projects'

--- Real Projects/Al-Kitab/accounts/views.py
+++ Real Projects/Al-Kitab/accounts/views.py
@@ -12,7 +12,13 @@ def store(request):
 
 def cart(request):
 
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+    context = {'items': items}
     return render(request, 'cart.html', context)
 
 
