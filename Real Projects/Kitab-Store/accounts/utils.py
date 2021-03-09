086f991233b5222fbfd73e47febceca1e320b895
Hacnine <hacninetusar@gmail.com>
2021-03-09 21:29:16 +0600

    'Projects'

--- Real Projects/Kitab-Store/accounts/utils.py
+++ Real Projects/Kitab-Store/accounts/utils.py
@@ -62,3 +62,38 @@ def cart_data(request):
     return {'cart_items': cart_items, 'order': order, 'items': items}
 
 
+def guest_order(request, data):
+    print('User is not logged in.')
+    print('COOKIES', request.COOKIES)
+
+    name = data['form']['name']
+    email = data['form']['email']
+    print('name', name)
+    print('email', email)
+
+    cookie_data = cookie_cart(request)
+    items = cookie_data['items']
+    customer, created = Customer.objects.get_or_create(
+        email=email,
+    )
+    customer.name = name
+    customer.save()
+
+    order = Order.objects.create(
+        customer=customer,
+        complete=False,
+    )
+
+    for item in items:
+        product = Product.objects.get(id=item['product']['id'])
+
+        order_item = OrderItem.objects.create(
+            product=product,
+            order=order,
+            quantity=item['quantity'],
+        )
+
+    return customer, order
+
+
+
--- Real Projects/Kitab-Store/accounts/views.py
+++ Real Projects/Kitab-Store/accounts/views.py
@@ -79,20 +79,26 @@ def process_order(request):
         if total == order.get_cart_total:
             order.complete = True
 
-        order.save()
-
-        if order.shipping == True:
-            ShippingAddress.objects.create(
-                customer=customer,
-                order=order,
-                address=data['shipping']['address'],
-                city=data['shipping']['city'],
-                state=data['shipping']['state'],
-                zipcode=data['shipping']['zipcode'],
-            )
-
     else:
-        print('User is not logged in.')
+        customer, order = guest_order(request, data)
+
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
+
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
+
+    order.save()
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+        )
 
     print('Data:', request.body)
     return JsonResponse('Payment complete', safe=False)
